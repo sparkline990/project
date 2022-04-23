efe3e33e988c009546dabd1f1f650b8821c4f6e0
unknown <sparkline990@gmail.com>
2022-04-23 16:33:46 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,75 @@
+"use strict";
+
+// console.log();
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number=10;
+
+// console.log(number);
+
+// const obj = {
+//     a:50
+// };
+
+// obj.a = 10;
+
+
+// const  widthBodyWidth  = 5000;
+// const lengthBodyLength = 4000;
+
+
+// console.log('Vehicle width ' + widthBodyWidth + ', Vehicle length ' +lengthBodyLength);
+
+
+// const arr = [1, 2, 3, 4];
+// const obj = {a: 1, b: 2};
+
+// console.log(arr[3]);
+
+/////////////////////////////////////////////////////////////////Allerts
+
+// alert('Hello');
+
+// const result = prompt('Are you 18?', '18');
+// console.log(result);
+
+// const answer = +prompt('Are you 18?', '18');
+// console.log(answer+5);
+
+
+
+// const answers = [];
+
+// answers[0] = prompt('How is your name?','');
+// answers[1] = prompt('How is your surname?','');
+// answers[2] = prompt('How old are you?','');
+
+// document.write(answers);
+
+//////////////////////////////////////////////////////////////Interpolation
+
+// const user = 'John'
+
+// alert(`Hello, ${user}`);
+
+/////////////////////////////////////////////////////////////Operators
+
+let incr = 10,
+    decr = 10;
+// incr++;
+// decr--;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+const isCheked1 = true,
+      isClose1 = true;
+console.log(isCheked1 && isClose1);
+
+const isCheked = true,
+      isClose = true;
+console.log(isCheked || isClose);
+
